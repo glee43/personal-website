d8f7520249617d20ac25b93c6f34f15d73500335
George <george_lee@brown.edu>
2019-01-02 16:29:55 -0800

    Updated Personal Projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,106 +4,123 @@ import { Carousel } from "react-responsive-carousel";
 import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class Portfolio extends Component {
-  listProjects(data) {
-    var projects = data.map((projects, index) => {
-      return (
-        <div>
-          <h5
-            style={{
-              color: "#666969",
-              position: "relative"
-            }}
-          >
-            {projects.title}
-          </h5>
-          <div className="player-wrapper">
-            <ReactPlayer
-              style={{
-                display: "flex",
-                width: "100%",
-                height: "100%",
-                justifyContent: "center"
-              }}
-              className="react-player"
-              url={projects.video}
-              playing
-              loop
-              controls
-              width="100%"
-              height="100%"
-            />
-          </div>
-          <p style={{ paddingBottom: 20, paddingTop: 10 }}>
-            {projects.category}
-          </p>
-        </div>
-      );
-    });
-    return projects;
-  }
-
-  render() {
-    if (this.props.data) {
-      var projects = this.listProjects(this.props.data.cs0150);
-      var projects2 = this.listProjects(this.props.data.cs0160);
-      var projects3 = this.listProjects(this.props.data.cs0330);
+    listProjects(data) {
+        var projects = data.map((projects, index) => {
+            return (
+                <div>
+                    <li>
+                        <a className="links" href={projects.url}>
+                            {projects.title}
+                        </a>
+                    </li>
+                    <div className="player-wrapper">
+                        <ReactPlayer
+                            style={{
+                                display: "flex",
+                                width: "100%",
+                                height: "100%",
+                                justifyContent: "center"
+                            }}
+                            className="react-player"
+                            url={projects.video}
+                            playing={false}
+                            loop
+                            controls
+                            width="100%"
+                            height="100%"
+                        />
+                    </div>
+                    <p style={{ paddingBottom: 20, paddingTop: 10 }}>
+                        {projects.category}
+                    </p>
+                </div>
+            );
+        });
+        return projects;
     }
 
-    return (
-      <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
-            <h2> CS0150: Introduction to Object-Oriented Programming </h2>
-            <div>
-              <Carousel
-                showIndicators
-                swipable
-                infiniteLoop
-                showThumbs={false}
-                style={{ overflow: "hidden", zIndex: 100000 }}
-              >
-                {projects}
-              </Carousel>
-            </div>
-            <h2> CS0160: Introduction to Data Structures and Algorithms </h2>
-            <div>
-              <Carousel
-                showIndicators
-                swipable
-                infiniteLoop
-                showThumbs={false}
-                style={{ overflow: "hidden" }}
-              >
-                {projects2}
-              </Carousel>
-            </div>
-            <h2> CS0330: Introduction to Computer Systems </h2>
-            <div>
-              <Carousel
-                showIndicators
-                swipable
-                infiniteLoop
-                showThumbs={false}
-                style={{ overflow: "hidden" }}
-              >
-                {projects3}
-              </Carousel>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+    render() {
+        if (this.props.data) {
+            var projects0 = this.listProjects(this.props.data.hackathons);
+            var projects1 = this.listProjects(this.props.data.cs0150);
+            var projects2 = this.listProjects(this.props.data.cs0160);
+            var projects3 = this.listProjects(this.props.data.cs0330);
+        }
+
+        return (
+            <section id="portfolio">
+                <div className="row">
+                    <div className="twelve columns collapsed">
+                        <h1>Check Out Some of My Works.</h1>
+                        <h2> Hackathons and Personal Projects </h2>
+                        <div>
+                            <Carousel
+                                showIndicators
+                                swipable
+                                infiniteLoop
+                                showThumbs={false}
+                                style={{ overflow: "hidden", zIndex: 100000 }}
+                            >
+                                {projects0}
+                            </Carousel>
+                        </div>
+                        <h2>
+                            {" "}
+                            CS0150: Introduction to Object-Oriented Programming{" "}
+                        </h2>
+                        <div>
+                            <Carousel
+                                showIndicators
+                                swipable
+                                infiniteLoop
+                                showThumbs={false}
+                                style={{ overflow: "hidden", zIndex: 100000 }}
+                            >
+                                {projects1}
+                            </Carousel>
+                        </div>
+                        <h2>
+                            {" "}
+                            CS0160: Introduction to Data Structures and
+                            Algorithms{" "}
+                        </h2>
+                        <div>
+                            <Carousel
+                                showIndicators
+                                swipable
+                                infiniteLoop
+                                showThumbs={false}
+                                style={{ overflow: "hidden" }}
+                            >
+                                {projects2}
+                            </Carousel>
+                        </div>
+                        <h2> CS0330: Introduction to Computer Systems </h2>
+                        <div>
+                            <Carousel
+                                showIndicators
+                                swipable
+                                infiniteLoop
+                                showThumbs={false}
+                                style={{ overflow: "hidden" }}
+                            >
+                                {projects3}
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Portfolio;
 
 {
-  /* Overlay component */
+    /* Overlay component */
 }
 {
-  /* <img alt={projects.title} src={projectImage} />
+    /* <img alt={projects.title} src={projectImage} />
             <div className="overlay">
               <div className="portfolio-item-meta">
                 <h5>{projects.title}</h5>
@@ -118,7 +135,7 @@ export default Portfolio;
 
 /* old layout */
 {
-  /* <div
+    /* <div
               id="portfolio-wrapper"
               className="bgrid-thirds s-bgrid-thirds cf"
             >
@@ -140,7 +157,7 @@ export default Portfolio;
             </div> */
 }
 {
-  /* <h2> Hackathon Projects </h2>
+    /* <h2> Hackathon Projects </h2>
             <div
               id="portfolio-wrapper"
               className="bgrid-thirds s-bgrid-thirds cf"
