8e61c9f4a539729a0f91e2c97da66bea8ea16f09
George <george_lee@brown.edu>
2019-10-02 00:06:41 -0400

    get rid of the resume section and changed the profile picture

--- src/App.js
+++ src/App.js
@@ -12,51 +12,51 @@ import Portfolio from "./Components/Portfolio";
 import Videos from "./Components/Videos";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      resumeData: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: "bar",
+            resumeData: {}
+        };
+
+        ReactGA.initialize("UA-110570651-1");
+        ReactGA.pageview(window.location.pathname);
+    }
+
+    getResumeData = () => {
+        $.ajax({
+            url: "/resumeData.json",
+            dataType: "json",
+            cache: false,
+            success: function(data) {
+                this.setState({ resumeData: data });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
     };
 
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
+    componentDidMount() {
+        this.getResumeData();
+    }
 
-  getResumeData = () => {
-    $.ajax({
-      url: "/resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  };
+    render() {
+        return (
+            <div className="App">
+                <Header data={this.state.resumeData.main} />
+                <About data={this.state.resumeData.main} />
+                {/* <Resume data={this.state.resumeData.resume} /> */}
+                <Portfolio data={this.state.resumeData.portfolio} />
 
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-
-        <Videos data={this.state.resumeData.videos} />
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
-      </div>
-    );
-  }
+                <Videos data={this.state.resumeData.videos} />
+                {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
+                <Contact data={this.state.resumeData.main} />
+                <Footer data={this.state.resumeData.main} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,103 +1,119 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 class Header extends Component {
-  render() {
-    if (this.props.data) {
-      var name = this.props.data.name;
-      var occupation = this.props.data.occupation;
-      var description = this.props.data.description;
-      var city = this.props.data.address.city;
-      var networks = this.props.data.social.map(function(network) {
+    render() {
+        if (this.props.data) {
+            var name = this.props.data.name;
+            var occupation = this.props.data.occupation;
+            var description = this.props.data.description;
+            var city = this.props.data.address.city;
+            var networks = this.props.data.social.map(function(network) {
+                return (
+                    <li key={network.name}>
+                        <a href={network.url}>
+                            <i className={network.className} />
+                        </a>
+                    </li>
+                );
+            });
+        }
+
         return (
-          <li key={network.name}>
-            <a href={network.url}>
-              <i className={network.className} />
-            </a>
-          </li>
-        );
-      });
-    }
+            <div>
+                <Particles
+                    style={{ position: "absolute" }}
+                    params={{
+                        particles: {
+                            number: {
+                                value: 100
+                            },
+                            size: {
+                                value: 5
+                            }
+                        },
+                        interactivity: {
+                            events: {
+                                onhover: {
+                                    enable: true,
+                                    mode: "repulse"
+                                }
+                            }
+                        }
+                    }}
+                />
+                <header id="home">
+                    <nav id="nav-wrap">
+                        <a
+                            className="mobile-btn"
+                            href="#nav-wrap"
+                            title="Show navigation"
+                        >
+                            Show navigation
+                        </a>
+                        <a
+                            className="mobile-btn"
+                            href="#home"
+                            title="Hide navigation"
+                        >
+                            Hide navigation
+                        </a>
 
-    return (
-      <div>
-        <Particles
-          style={{ position: "absolute" }}
-          params={{
-            particles: {
-              number: {
-                value: 100
-              },
-              size: {
-                value: 5
-              }
-            }
-          }}
-        />
-        <header id="home">
-          <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-              Show navigation
-            </a>
-            <a className="mobile-btn" href="#home" title="Hide navigation">
-              Hide navigation
-            </a>
-
-            <ul id="nav" className="nav">
-              <li className="current">
-                <a className="smoothscroll" href="#home">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#about">
-                  About
-                </a>
-              </li>
-              <li>
+                        <ul id="nav" className="nav">
+                            <li className="current">
+                                <a className="smoothscroll" href="#home">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#about">
+                                    About Me
+                                </a>
+                            </li>
+                            {/* <li>
                 <a className="smoothscroll" href="#resume">
                   Resume
                 </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#portfolio">
-                  Works
-                </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#videos">
-                  Videos
-                </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
+              </li> */}
+                            <li>
+                                <a className="smoothscroll" href="#portfolio">
+                                    Works
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#videos">
+                                    Videos
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#contact">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
 
-          <div className="row banner">
-            <div className="banner-text">
-              <h1 className="responsive-headline">{name}</h1>
-              <h3>
-                <span>
-                  I'm Currently a {occupation} {description}
-                </span>
-              </h3>
-              <hr />
-              <ul className="social">{networks}</ul>
+                    <div className="row banner">
+                        <div className="banner-text">
+                            <h1 className="responsive-headline">{name}</h1>
+                            <h3>
+                                <span>
+                                    I'm Currently a {occupation} {description}
+                                </span>
+                            </h3>
+                            <hr />
+                            <ul className="social">{networks}</ul>
+                        </div>
+                    </div>
+
+                    <p className="scrolldown">
+                        <a className="smoothscroll" href="#about">
+                            <i className="icon-down-circle" />
+                        </a>
+                    </p>
+                </header>
             </div>
-          </div>
-
-          <p className="scrolldown">
-            <a className="smoothscroll" href="#about">
-              <i className="icon-down-circle" />
-            </a>
-          </p>
-        </header>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Header;
